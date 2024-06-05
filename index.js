7da197204354071709e70ce71e0c27ada29daa48
amdreh <amdreh@gmail.com>
2024-06-05 17:49:44 -0300

    auto

--- index.js
+++ index.js
@@ -3,3 +3,18 @@
 //     // Altera a cor de fundo para preto
     // $('body').css('background-color', 'black');
 // });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var hamburguerButton = document.getElementById('hamburguer-button');
+    var menu = new bootstrap.Collapse(document.getElementById('menu'), {toggle: false});
+
+    document.addEventListener('click', function (event) {
+        var target = event.target;
+        var menuElement = document.getElementById('menu');
+
+        // Verifica se o menu está visível, se o clique foi fora do menu e do botão hamburguer, ou se foi em um item do menu
+        if (menu && menu._isShown && (!menuElement.contains(target) || target.classList.contains('nav-link')) && target !== hamburguerButton) {
+            menu.hide();
+        }
+    });
+});
